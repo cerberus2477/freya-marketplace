24083e1ba9f781133695453b96f6265014f84cb0
cerberus2477 <tabor.tunde@gmail.com>
2025-04-30 07:36:08 +0200

    added loading spinner to pages

---
+++ FreyaMarketplace/ViewModel/FontAwesomeIcons.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FreyaMarketplace.ViewModel
+{
+    public static class FontAwesomeIcons
+    {
+        public static string Location => "\uf041";
+        public static string Clock => "\uf017";
+    }
+}
